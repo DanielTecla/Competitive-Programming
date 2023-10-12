c38d4256023f892cbe683ff7a3a9c2af4ca1a7e9
DanielTecla <daniel.ctecla@gmail.com>
2023-10-11 22:43:54 -0600

    update

---
+++ Algoritmia Problems/469926B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MaxR = 100;
+const int MaxC = 2*100 - 1;
+
+void solution(){
+    char grid [MaxR][MaxC];
+    
+}
+
+int main(){
+
+    solution();
+    return 0;
+
+}
---
+++ Algoritmia Problems/A. Beautiful Matrix 263A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solution(){
+    int row,column,l;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            cin>>l;
+            if(l == 1){
+                row = i;
+                column = j;
+            }
+        }
+    }
+
+    int num = 0;
+    num += abs(3 - column);
+    num += abs(3 - row);
+
+    cout<<num; 
+    
+}
+
+int main(){
+    solution();
+    return 0;
+}
