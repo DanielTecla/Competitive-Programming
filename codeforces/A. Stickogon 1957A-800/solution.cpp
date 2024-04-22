1df3755d724f055a2688e76a7b61795849c2bead
danielctecla <daniel.ctecla@gmail.com>
2024-04-22 12:22:52 -0600

    A. Stickogon solved

---
+++ codeforces/A. Stickogon 1957A-800/solution.cpp
@@ -0,0 +1,54 @@
+//https://codeforces.com/contest/1957/problem/A
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define ll long long
+#define lli long long int
+#define ld long double
+#define fore(i,a,b) for (int i = a; i < b; i++)
+#define forr(i,a,b) for (int i = a; i > b; i--)
+#define forn(i,n) for (int i = 0; i < n; i++)
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define fast_io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef map<int, int> mii;
+
+void solve(){
+    unordered_map<int,int> map;
+    int n;cin>>n;
+    int count = 0;
+
+    forn(i,n){
+        int element; cin>>element;
+        if(map.count(element)>0) map[element]++;
+        else map[element] = 1; 
+    }
+    
+    for(const auto elem: map){
+        if(elem.second >= 3){
+            int num = elem.second/3;
+            count += num;
+        }
+        
+    }
+    cout<<count<<endl;
+}
+
+int main() {
+    fast_io
+    int t; cin>>t;
+    while(t--)solve();
+}
